491f7a3484453b64d0248e23b4b979ad8bdf178b
FirstFox3 <taylsmit811@gmail.com>
2026-06-13 03:04:23 -0400

    Cleanup

--- widget/Cat.tsx
+++ widget/Cat.tsx
@@ -7,12 +7,12 @@ let [file, setFile] = createState("none")
 let leftNext: Boolean = true
 let image: Gtk.Image
 
-function onKeyPressed(..._ignored: any[]) {
+function onKeyPressed() {
 	setFile(leftNext ? "left" : "right")
 	leftNext = !leftNext
 }
 
-function onKeyReleased(..._ignored: any[]) {
+function onKeyReleased() {
 	setFile("none")
 }
 
@@ -24,19 +24,13 @@ subprocess("libinput debug-events", (stdout: string) => {
 export default function Cat() {
 	const { BOTTOM, RIGHT } = Astal.WindowAnchor
 
-	// When nothing is pressed, do nothing. Else, alternate between left and right.
-
-	
 	return (<window
 		visible
 		application={app}
 		layer={Astal.Layer.OVERLAY}
-		// keymode={Astal.Keymode.ON_DEMAND}
 		anchor={BOTTOM | RIGHT}
 		class="Cat"
 	>
-		<Gtk.EventControllerKey onKeyPressed={onKeyPressed} onKeyReleased={onKeyReleased} />
-
 		<image $={self => image = self} file={file(val => `assets/${val}.png`)} pixelSize={256} />
 	</window>)
 }
